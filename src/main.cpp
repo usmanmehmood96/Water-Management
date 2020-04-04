69ea29dd749e34c5f08df116c3dce4b5f206cf3e
Usman Mehmood <usmanmehmood55@gmail.com>
2020-04-04 14:10:08 +0500

    Blynk loaded

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
 
-void setup() {
-  // put your setup code here, to run once:
+char auth[] = "ESgACXgZNSgNR3qQj2yA8Vbf1KrMW1yc";
+char ssid[] = "Imran";
+char pass[] = "11458900";
+
+void setup()
+{
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  Blynk.run();
 }
